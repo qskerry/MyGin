354ff05f8a1d4acb528b507a6e5353cbcc566353
wangbo <qskerry@163.com>
2022-03-05 13:57:57 +0800

    http server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"MyGin/framework"
+	"net/http"
+)
+
+
+
+
+
+func main() {
+	server := &http.Server{
+		// 自定义的请求核心处理函数
+		Handler: framework.NewCore(),
+		// 请求监听地址
+		Addr:    ":8080",
+	}
+	server.ListenAndServe()
+}
+
+
