dd3abf4370d9a892f910d88bcb598ca9c906d987
wangbo <qskerry@163.com>
2022-03-06 09:20:41 +0800

    context 02

--- framework/context.go
+++ framework/context.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"sync"
+	"time"
 )
 
 type Context struct{
@@ -13,11 +14,11 @@ type Context struct{
 	handler           ControllerHandler
 
 	writerMux         *sync.Mutex
+	hasTimeOut        bool
 
 }
 
 func NewContext(r *http.Request, w http.ResponseWriter) *Context{
-
 	return &Context{
 		request:        r,
 		responseWriter: w,
@@ -26,12 +27,47 @@ func NewContext(r *http.Request, w http.ResponseWriter) *Context{
 	}
 }
 
+func (ctx *Context)WriteMux() *sync.Mutex{
+	return ctx.writerMux
+}
+
+func (ctx *Context)GetRequest() *http.Request{
+	return ctx.request
+}
+
+func (ctx *Context)GetResponse() http.ResponseWriter{
+	return ctx.responseWriter
+}
+
+func (ctx *Context)SetHasTimeOut(){
+	ctx.hasTimeOut = true
+}
+
+func (ctx *Context)HasTimeOut() bool{
+	return ctx.hasTimeOut
+}
 
 
 func (ctx *Context) BaseContext() context.Context{
 	return ctx.request.Context()
 }
 
-func (ctx *Context) Done() <-chan struct{}{
+func (ctx *Context)Deadline()(deadline time.Time, ok bool){
+	return ctx.BaseContext().Deadline()
+}
+
+func (ctx *Context)Done() <-chan struct{}{
 	return ctx.BaseContext().Done()
+}
+
+func (ctx *Context)Err() error{
+	return ctx.BaseContext().Err()
+}
+
+func (ctx *Context)Value(key interface{}) interface{}{
+	return ctx.BaseContext().Value(key)
+}
+
+func (ctx *Context)QueryInt(key string,def int){
+	
 }
