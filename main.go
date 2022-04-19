0e38beb5b2fc6b75a05b4353dc67539ee07af935
wangbo <bo6.wang@ly.com>
2022-04-19 16:14:07 +0800

    修改main

--- main.go
+++ main.go
@@ -6,6 +6,62 @@ import (
 	"net/http"
 )
 
+func UserLoginController(c *framework.Context) error {
+	c.Json(200, "ok, UserLoginController")
+	return nil
+}
+
+func SubjectAddController(c *framework.Context) error {
+	c.Json(200, "ok, SubjectAddController")
+	return nil
+}
+
+func SubjectListController(c *framework.Context) error {
+	c.Json(200, "ok, SubjectListController")
+	return nil
+}
+
+func SubjectDelController(c *framework.Context) error {
+	c.Json(200, "ok, SubjectDelController")
+	return nil
+}
+
+func SubjectUpdateController(c *framework.Context) error {
+	c.Json(200, "ok, SubjectUpdateController")
+	return nil
+}
+
+func SubjectGetController(c *framework.Context) error {
+	c.Json(200, "ok, SubjectGetController")
+	return nil
+}
+
+func SubjectNameController(c *framework.Context) error {
+	c.Json(200, "ok, SubjectNameController")
+	return nil
+}
+
+// 注册路由规则
+func registerRouter(core *framework.Core) {
+	// 静态路由+HTTP方法匹配
+	core.Get("/user/login", UserLoginController)
+
+	// 批量通用前缀
+	subjectApi := core.Group("/subject")
+	{
+		// 动态路由
+		subjectApi.Delete("/:id", SubjectDelController)
+		subjectApi.Put("/:id", SubjectUpdateController)
+		subjectApi.Get("/:id", SubjectGetController)
+		subjectApi.Get("/list/all", SubjectListController)
+
+		subjectInnerApi := subjectApi.Group("/info")
+		{
+			subjectInnerApi.Get("/name", SubjectNameController)
+		}
+	}
+}
+
 func main() {
 	core := framework.NewCore()
 	// core.Use(
